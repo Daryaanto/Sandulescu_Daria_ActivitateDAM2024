2915d6e6b74be5b7235c933149b6a099e441edf6
Daryaanto <91839182+Daryaanto@users.noreply.github.com>
2024-10-13 15:57:05 +0300

    seminar 1

---
+++ Seminar1_1099/Seminar1_1099/app/src/main/java/com/example/seminar1_1099/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.seminar1_1099;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        console.log("change")
+    }
+}
