473e453e323cceb04394e6a6c93f5e11317d55d4
Daria Sandulescu <dariaantonia.sandulescu@gmail.com>
2024-10-16 13:32:18 +0300

    Seminar 3

---
+++ seminar3_1099/app/src/main/java/com/example/seminar3_1099/secondActivity.java
@@ -0,0 +1,35 @@
+package com.example.seminar3_1099;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class secondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+       Intent it =  getIntent();
+       String str = it.getStringExtra("text");
+       int nr = it.getIntExtra("text1",0);
+       int nr2 = it.getIntExtra("text2",0);
+int sum = nr+nr2;
+
+        Toast.makeText(this, str +" "+ sum, Toast.LENGTH_LONG).show();
+
+    }
+}
